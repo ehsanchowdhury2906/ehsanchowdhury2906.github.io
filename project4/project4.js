1c746351449c50acc08bd3d844998626c9cd8b8a
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2025-03-27 15:12:02 -0400

    Update project4.js

--- project4/project4.js
+++ project4/project4.js
@@ -1,1 +1,16 @@
+let carbtn = document.querySelector(".carbtn")
+let robtBtn = document.querySelector(".robtBtn")
+function scrollgallery(pexels){
+    let gallerycontainer = document.querySelector(".gallerycontainer")
+    gallerycontainer.scrollBy({
+        left:pexels,
+        behavior:"smooth"
+    })
+}
 
+carbtn.addEventListener("click", function(){
+    scrollgallery(1200)
+})
+robtBtn.addEventListener("click", function(){
+    scrollgallery(-1200)
+})
